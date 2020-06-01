86f8c6f4f16a0e5e6c3dfabea2ea3c207d9e1e81
Judaman <judagru@gmail.com>
2020-06-01 18:46:35 -0400

    com

--- sendToZeeMaps.js
+++ sendToZeeMaps.js
@@ -23,36 +23,25 @@ module.exports = {
 
     await page.type('input#u_email', 'daniel@dmvhometherapy.com');
     await page.type('input#u_passwd', 'yudidaniel12');
-
     await page.waitForSelector('body > div:nth-child(28) > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button:nth-child(1)');
     await page.click('body > div:nth-child(28) > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button:nth-child(1)');
     await page.waitForNavigation();
-
     await page.evaluate(() => {
       let select = document.querySelector('a.mapmenuitem.upload');
       select.click();
 
     });
-
-    var newEntry = "Name	Street	City	State	Country	Zipcode	Color \n" + name + "	" + street + "	" + city + "	" + state + "	" + country + "	" + zipcode + "	" + color
-
-
+    var newEntry = "Name	Street	City	State	Country	Zipcode	Color \n" + name + "	" + street + "	" + city + "	" + state + "	" + country + "	" + zipcode + "	" + color;
+    await page.waitFor(1000);
+    await page.click('#ui-id-5');
     await page.waitForSelector("textarea#textcsv")
     await page.type('textarea#textcsv', newEntry);
     await page.waitFor(1000);
     await page.click('#csvtextsubmit');
-
-
     await page.waitFor(1000);
     await page.waitForSelector("#uploadconfirmsubmit > span")
     await page.click('#uploadconfirmsubmit > span');
-
     await page.waitFor(1000);
-    /*  await page.screenshot({
-        path: 'example.png'
-      });*/
-
     await browser.close();
-
   }
 }
